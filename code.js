856bbddd36e37c18bb037135c138ff33614b81d5
Ishani Mukherjee <ishanibhattacharjee@gmail.com>
2017-06-16 13:06:00 -0700

    search location on map implemented

--- code.js
+++ code.js
@@ -1,52 +1,68 @@
-      var map;
+var map;
       function initMap() {
+        geocoder = new google.maps.Geocoder();
         map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 2,
-          center: new google.maps.LatLng(2.8,-187.3),
-          mapTypeId: 'terrain'
+          zoom: 10,
+          center: new google.maps.LatLng(37.5985,-122.3872),
         });
-
-        // Create a <script> tag and set the USGS URL as the source.
-        var script = document.createElement('script');
-        // This example uses a local copy of the GeoJSON stored at
-        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-        script.src = 'mapdata.json';
-        document.getElementsByTagName('head')[0].appendChild(script);
-        }
-
-      // Loop through the results array and place a marker for each
-      // set of coordinates.
-      window.eqfeed_callback = function(results) {
-        var markers = [];
-        var largeInfowindow = new google.maps.InfoWindow();
-        var bounds = new google.maps.LatLngBounds();
-        for (var i = 0; i < results.features.length; i++) {
-          var coords = results.features[i].geometry.coordinates;
-          var latLng = new google.maps.LatLng(coords[1],coords[0]);
-          var info = results.features[i].properties.place;
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map,
-            title: info
-          });
-          markers.push(marker);
-          marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
-          });
-          bounds.extend(markers[i].position);
-        }
-        map.fitBounds(bounds);
       }
 
-  function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-          infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
-          infowindow.open(map, marker);
-          // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick',function(){
-            infowindow.setMarker = null;
-          });
+markers=[];
+var locations = [
+          {title: 'San Francisco', location: {lat:  37.77493, lng: -122.419416}},
+          {title: '22nd Street', location: {lat: 40.7444883, lng: -73.9949465}},
+          {title: 'Bayshore', location: {lat: 40.7347062, lng: -73.9895759}},
+          {title: 'San Bruno', location: {lat: 40.7281777, lng: -73.984377}},
+          {title: 'Milbrae Transit', location: {lat: 40.7195264, lng: -74.0089934}},
+          {title: 'Broadway', location: {lat: 40.7180628, lng: -73.9961237}}
+        ];
+  $(document).ready(function() 
+ {
+    $('li').click(function(e) 
+    { 
+      document.getElementById('address').value = $(this).text();
+      codeAddress();
+      
+    });
+ });
+
+function codeAddress() {
+    var address = document.getElementById('address').value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      clearMarkers();
+      if (status == 'OK') {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+        markers.push(marker);
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+
+    // Sets the map on all markers in the array.
+      function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
         }
+      }
+
+     // Removes the markers from the map, but keeps them in the array.
+      function clearMarkers() {
+        setMapOnAll(null);
+      }
+
+      // Shows any markers currently in the array.
+      function showMarkers() {
+        setMapOnAll(map);
+      }
+
+      // Deletes all markers in the array by removing references to them.
+      function deleteMarkers() {
+        clearMarkers();
+        markers = [];
       }
