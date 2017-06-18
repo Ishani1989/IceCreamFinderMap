bf268e22599d233c4eba2177dff9216016a00d43
Ishani Mukherjee <ishanibhattacharjee@gmail.com>
2017-06-18 02:40:38 -0700

    added ice cream center  search functionality using Google maps and Foursquare API

---
+++ README.md
@@ -0,0 +1,26 @@
+# Cal Train Ice cream finder app
+
+This project is written in Javascript and HTML and uses 3rd party APIs such as Google maps and Foursquare to list the names of caltrain stations between San Francisco and San Jose and get the nearest ice cream parlours for each place on selection. The index.html file when run in browser displays the list of caltrain stations on the left with their respective maps on the right.
+
+## Dependency
+
+knockout-3.4.2.js<br />
+HTML 5<br />
+Javascript version 1.7<br />
+
+
+## How to run
+
+Run the index.html file to view the project.
+
+## Tested Env
+
+Tested in Chrome version 57.0.2987.133
+
+## History
+
+First checkin done on June 17, 2017
+
+## Credits
+
+ishanibhattacharjee@gmail.com
--- code.js
+++ code.js
@@ -1,68 +1,274 @@
 var map;
-      function initMap() {
-        geocoder = new google.maps.Geocoder();
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 10,
-          center: new google.maps.LatLng(37.5985,-122.3872),
-        });
-      }
+// Create a new blank array for all the listing markers.
+var markers = [];
 
-markers=[];
-var locations = [
-          {title: 'San Francisco', location: {lat:  37.77493, lng: -122.419416}},
-          {title: '22nd Street', location: {lat: 40.7444883, lng: -73.9949465}},
-          {title: 'Bayshore', location: {lat: 40.7347062, lng: -73.9895759}},
-          {title: 'San Bruno', location: {lat: 40.7281777, lng: -73.984377}},
-          {title: 'Milbrae Transit', location: {lat: 40.7195264, lng: -74.0089934}},
-          {title: 'Broadway', location: {lat: 40.7180628, lng: -73.9961237}}
-        ];
-  $(document).ready(function() 
- {
-    $('li').click(function(e) 
-    { 
-      document.getElementById('address').value = $(this).text();
-      codeAddress();
-      
+var locations = [{
+        title: 'San Francisco',
+        location: {
+            lat: 37.774929,
+            lng: -122.419416
+        }
+    },
+    {
+        title: '22nd Street',
+        location: {
+            lat: 37.755845,
+            lng: -122.412226
+        }
+    },
+    {
+        title: 'Bayshore',
+        location: {
+            lat: 37.467040,
+            lng: -122.151928
+        }
+    },
+    {
+        title: 'South San Francisco',
+        location: {
+            lat: 37.654656,
+            lng: -122.407750
+        }
+    },
+    {
+        title: 'San Bruno',
+        location: {
+            lat: 37.630490,
+            lng: -122.411084
+        }
+    },
+    {
+        title: 'Millbrae Transit Center',
+        location: {
+            lat: 37.600156,
+            lng: -122.386936
+        }
+    },
+    {
+        title: 'Broadway Weekend only',
+        location: {
+            lat: 37.795939,
+            lng: -122.421890
+        }
+    },
+    {
+        title: 'Burlingame',
+        location: {
+            lat: 37.577870,
+            lng: -122.348090
+        }
+    },
+    {
+        title: 'San Mateo',
+        location: {
+            lat: 37.562992,
+            lng: -122.325525
+        }
+    },
+    {
+        title: 'Hillsdale',
+        location: {
+            lat: 37.781611,
+            lng: -122.409347
+        }
+    },
+    {
+        title: 'Belmont',
+        location: {
+            lat: 37.525016,
+            lng: -122.266873
+        }
+    },
+    {
+        title: 'San Carlos',
+        location: {
+            lat: 37.507159,
+            lng: -122.260522
+        }
+    },
+    {
+        title: 'Redwood City',
+        location: {
+            lat: 37.507159,
+            lng: -122.260522
+        }
+    },
+    {
+        title: 'Atherton Weekend only',
+        location: {
+            lat: 37.507159,
+            lng: -122.260522
+        }
+    },
+    {
+        title: 'Menlo Park',
+        location: {
+            lat: 37.507159,
+            lng: -122.260522
+        }
+    },
+    {
+        title: 'Palo Alto',
+        location: {
+            lat: 37.507159,
+            lng: -122.260522
+        }
+    },
+    {
+        title: 'California Ave.',
+        location: {
+            lat: 37.507159,
+            lng: -122.260522
+        }
+    },
+    {
+        title: 'San Antonio',
+        location: {
+            lat: 37.507159,
+            lng: -122.260522
+        }
+    },
+    {
+        title: 'Mountain View',
+        location: {
+            lat: 37.386052,
+            lng: -122.083851
+        }
+    },
+    {
+        title: 'Sunnyvale',
+        location: {
+            lat: 37.507159,
+            lng: -122.260522
+        }
+    },
+    {
+        title: 'Lawrence',
+        location: {
+            lat: 37.507159,
+            lng: -122.260522
+        }
+    },
+    {
+        title: 'Santa Clara',
+        location: {
+            lat: 37.507159,
+            lng: -122.260522
+        }
+    },
+    {
+        title: 'San Jose',
+        location: {
+            lat: 37.338208,
+            lng: -121.886329
+        }
+    }
+];
+
+function initMap() {
+    // Constructor creates a new map - only center and zoom are required.
+    var geocoder = new google.maps.Geocoder();
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 37.5539,
+            lng: -122.3136
+        },
+        zoom: 11
     });
- });
+    // These are the real estate listings that will be shown to the user.
+    // Normally we'd have these in a database instead.
 
-function codeAddress() {
-    var address = document.getElementById('address').value;
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      clearMarkers();
-      if (status == 'OK') {
-        map.setCenter(results[0].geometry.location);
+    var largeInfowindow = new google.maps.InfoWindow();
+    var bounds = new google.maps.LatLngBounds();
+    // The following group uses the location array to create an array of markers on initialize.
+    for (var i = 0; i < locations.length; i++) {
+        // Get the position from the location array.
+        var location = locations[i].location;
+        var title = locations[i].title;
         var marker = new google.maps.Marker({
             map: map,
-            position: results[0].geometry.location
+            position: location,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
         });
         markers.push(marker);
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-  }
+        marker.addListener('click', function() {
+          populateInfoWindow(this, largeInfowindow);
+        });
+    }
 
+} //end initMap function
 
-    // Sets the map on all markers in the array.
-      function setMapOnAll(map) {
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(map);
+function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          infowindow.marker = marker;
+          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.open(map, marker);
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick',function(){
+            infowindow.setMarker = null;
+          });
         }
-      }
+}
+$(document).ready(function() {
+    $('.stationnames li').click(function() {
+        var location = $(this).text();
+        var loc = "";
+        //alert($(this).text());
+        for (var i = 0; i < locations.length; i++) {
+            if (locations[i].title === location) {
+                loc = locations[i].location;
+                console.log('Found Location');
+                console.log(loc);
+                break;
+            }
+        }
+        map.setCenter(loc);
+        map.setZoom(15);
+        date = "20161016"
+        var token = "EGIVZV20P2M153FGX3NIDOIKWX1QOXXUNEOQPZNFKGWUIVMF"
+        var foursqrurl="https://api.foursquare.com/v2/venues/search?oauth_token="+token+"&v="+date+"&ll="+loc.lat+','+loc.lng+"&query=ice%20cream&intent=checkin&limit=5&radius=2000";
+        console.log(foursqrurl);
 
-     // Removes the markers from the map, but keeps them in the array.
-      function clearMarkers() {
-        setMapOnAll(null);
-      }
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": foursqrurl,
+            "method": "GET"
+        }
 
-      // Shows any markers currently in the array.
-      function showMarkers() {
-        setMapOnAll(map);
-      }
+        $.ajax(settings).done(function(response) {
+          for(var ind in response.response.venues){
+            console.log("*****************");
+            venue = response.response.venues[ind];
+            var resname = venue.name;
+            var reslat = venue.location.lat;
+            var reslng = parseFloat(venue.location.lng);
+            var resloc = {
+              lat: reslat,
+              lng: reslng
+            }
+            var largeInfowindow = new google.maps.InfoWindow(); 
+            var marker = new google.maps.Marker({
+              map: map,
+              position: resloc,
+              title: resname,
+              icon: 'mapicon.png'
+            });
+            markers.push(marker);
+            marker.addListener('click', function() {
+              populateInfoWindow(this, largeInfowindow);
+            });
+            console.log(resname);
+            console.log('reslat - ' + reslng );
+            console.log('reslng - ' + resloc.lng);
+          }//end for
+        });//end ajax call
+    });
 
-      // Deletes all markers in the array by removing references to them.
-      function deleteMarkers() {
-        clearMarkers();
-        markers = [];
-      }
+
+
+
+});
